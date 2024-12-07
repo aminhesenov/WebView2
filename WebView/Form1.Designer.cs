351ca6b9a539e7fe672405604142080c736f220d
Amin Hasanov <44285676+aminhesenov@users.noreply.github.com>
2024-12-07 16:21:07 +0400

    webview

---
+++ WebView/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WebView
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            menuStrip1 = new MenuStrip();
+            sosialMediaToolStripMenuItem = new ToolStripMenuItem();
+            instagramToolStripMenuItem = new ToolStripMenuItem();
+            youtubeToolStripMenuItem = new ToolStripMenuItem();
+            facebookToolStripMenuItem = new ToolStripMenuItem();
+            telegramToolStripMenuItem = new ToolStripMenuItem();
+            youtubeToolStripMenuItem1 = new ToolStripMenuItem();
+            youtubeToolStripMenuItem2 = new ToolStripMenuItem();
+            alətlərToolStripMenuItem = new ToolStripMenuItem();
+            tamEkranToolStripMenuItem = new ToolStripMenuItem();
+            kiçikEkranToolStripMenuItem = new ToolStripMenuItem();
+            kopyalamaqToolStripMenuItem = new ToolStripMenuItem();
+            yapışdırmaqToolStripMenuItem = new ToolStripMenuItem();
+            əlaqəToolStripMenuItem = new ToolStripMenuItem();
+            xəritəToolStripMenuItem = new ToolStripMenuItem();
+            telefonToolStripMenuItem = new ToolStripMenuItem();
+            emailToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.BackColor = Color.Black;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = SystemColors.Window;
+            webView21.Dock = DockStyle.Fill;
+            webView21.Location = new Point(0, 24);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(658, 412);
+            webView21.Source = new Uri("https://www.google.az/", UriKind.Absolute);
+            webView21.TabIndex = 0;
+            webView21.ZoomFactor = 1D;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.Black;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { sosialMediaToolStripMenuItem, alətlərToolStripMenuItem, əlaqəToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(658, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // sosialMediaToolStripMenuItem
+            // 
+            sosialMediaToolStripMenuItem.BackColor = Color.Black;
+            sosialMediaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { instagramToolStripMenuItem, youtubeToolStripMenuItem, facebookToolStripMenuItem, telegramToolStripMenuItem, youtubeToolStripMenuItem1, youtubeToolStripMenuItem2 });
+            sosialMediaToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            sosialMediaToolStripMenuItem.Name = "sosialMediaToolStripMenuItem";
+            sosialMediaToolStripMenuItem.Size = new Size(85, 20);
+            sosialMediaToolStripMenuItem.Text = "Sosial media";
+            // 
+            // instagramToolStripMenuItem
+            // 
+            instagramToolStripMenuItem.BackColor = Color.Black;
+            instagramToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            instagramToolStripMenuItem.Image = (Image)resources.GetObject("instagramToolStripMenuItem.Image");
+            instagramToolStripMenuItem.Name = "instagramToolStripMenuItem";
+            instagramToolStripMenuItem.Size = new Size(127, 22);
+            instagramToolStripMenuItem.Text = "İnstagram";
+            instagramToolStripMenuItem.Click += instagramToolStripMenuItem_Click;
+            // 
+            // youtubeToolStripMenuItem
+            // 
+            youtubeToolStripMenuItem.BackColor = Color.Black;
+            youtubeToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            youtubeToolStripMenuItem.Image = (Image)resources.GetObject("youtubeToolStripMenuItem.Image");
+            youtubeToolStripMenuItem.Name = "youtubeToolStripMenuItem";
+            youtubeToolStripMenuItem.Size = new Size(127, 22);
+            youtubeToolStripMenuItem.Text = "Youtube";
+            youtubeToolStripMenuItem.Click += youtubeToolStripMenuItem_Click;
+            // 
+            // facebookToolStripMenuItem
+            // 
+            facebookToolStripMenuItem.BackColor = Color.Black;
+            facebookToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            facebookToolStripMenuItem.Image = (Image)resources.GetObject("facebookToolStripMenuItem.Image");
+            facebookToolStripMenuItem.Name = "facebookToolStripMenuItem";
+            facebookToolStripMenuItem.Size = new Size(127, 22);
+            facebookToolStripMenuItem.Text = "Facebook";
+            facebookToolStripMenuItem.Click += facebookToolStripMenuItem_Click;
+            // 
+            // telegramToolStripMenuItem
+            // 
+            telegramToolStripMenuItem.BackColor = Color.Black;
+            telegramToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            telegramToolStripMenuItem.Image = (Image)resources.GetObject("telegramToolStripMenuItem.Image");
+            telegramToolStripMenuItem.Name = "telegramToolStripMenuItem";
+            telegramToolStripMenuItem.Size = new Size(127, 22);
+            telegramToolStripMenuItem.Text = "Telegram";
+            telegramToolStripMenuItem.Click += telegramToolStripMenuItem_Click;
+            // 
+            // youtubeToolStripMenuItem1
+            // 
+            youtubeToolStripMenuItem1.Name = "youtubeToolStripMenuItem1";
+            youtubeToolStripMenuItem1.Size = new Size(127, 22);
+            youtubeToolStripMenuItem1.Visible = false;
+            youtubeToolStripMenuItem1.Click += youtubeToolStripMenuItem1_Click;
+            // 
+            // youtubeToolStripMenuItem2
+            // 
+            youtubeToolStripMenuItem2.Name = "youtubeToolStripMenuItem2";
+            youtubeToolStripMenuItem2.Size = new Size(127, 22);
+            youtubeToolStripMenuItem2.Visible = false;
+            // 
+            // alətlərToolStripMenuItem
+            // 
+            alətlərToolStripMenuItem.BackColor = Color.Black;
+            alətlərToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { tamEkranToolStripMenuItem, kiçikEkranToolStripMenuItem, kopyalamaqToolStripMenuItem, yapışdırmaqToolStripMenuItem });
+            alətlərToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            alətlərToolStripMenuItem.Name = "alətlərToolStripMenuItem";
+            alətlərToolStripMenuItem.Size = new Size(53, 20);
+            alətlərToolStripMenuItem.Text = "Alətlər";
+            // 
+            // tamEkranToolStripMenuItem
+            // 
+            tamEkranToolStripMenuItem.BackColor = Color.Black;
+            tamEkranToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            tamEkranToolStripMenuItem.Name = "tamEkranToolStripMenuItem";
+            tamEkranToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.F;
+            tamEkranToolStripMenuItem.Size = new Size(182, 22);
+            tamEkranToolStripMenuItem.Text = "Tam ekran";
+            tamEkranToolStripMenuItem.Click += tamEkranToolStripMenuItem_Click;
+            // 
+            // kiçikEkranToolStripMenuItem
+            // 
+            kiçikEkranToolStripMenuItem.BackColor = Color.Black;
+            kiçikEkranToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            kiçikEkranToolStripMenuItem.Name = "kiçikEkranToolStripMenuItem";
+            kiçikEkranToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.K;
+            kiçikEkranToolStripMenuItem.Size = new Size(182, 22);
+            kiçikEkranToolStripMenuItem.Text = "Kiçik ekran";
+            kiçikEkranToolStripMenuItem.Click += kiçikEkranToolStripMenuItem_Click;
+            // 
+            // kopyalamaqToolStripMenuItem
+            // 
+            kopyalamaqToolStripMenuItem.BackColor = Color.Black;
+            kopyalamaqToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            kopyalamaqToolStripMenuItem.Name = "kopyalamaqToolStripMenuItem";
+            kopyalamaqToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.C;
+            kopyalamaqToolStripMenuItem.Size = new Size(182, 22);
+            kopyalamaqToolStripMenuItem.Text = "Kopyalamaq";
+            // 
+            // yapışdırmaqToolStripMenuItem
+            // 
+            yapışdırmaqToolStripMenuItem.BackColor = Color.Black;
+            yapışdırmaqToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            yapışdırmaqToolStripMenuItem.Name = "yapışdırmaqToolStripMenuItem";
+            yapışdırmaqToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.V;
+            yapışdırmaqToolStripMenuItem.Size = new Size(182, 22);
+            yapışdırmaqToolStripMenuItem.Text = "Yapışdırmaq";
+            // 
+            // əlaqəToolStripMenuItem
+            // 
+            əlaqəToolStripMenuItem.BackColor = Color.Black;
+            əlaqəToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { xəritəToolStripMenuItem, telefonToolStripMenuItem, emailToolStripMenuItem });
+            əlaqəToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            əlaqəToolStripMenuItem.Name = "əlaqəToolStripMenuItem";
+            əlaqəToolStripMenuItem.Size = new Size(50, 20);
+            əlaqəToolStripMenuItem.Text = "Əlaqə";
+            // 
+            // xəritəToolStripMenuItem
+            // 
+            xəritəToolStripMenuItem.BackColor = Color.Black;
+            xəritəToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            xəritəToolStripMenuItem.Image = (Image)resources.GetObject("xəritəToolStripMenuItem.Image");
+            xəritəToolStripMenuItem.Name = "xəritəToolStripMenuItem";
+            xəritəToolStripMenuItem.Size = new Size(180, 22);
+            xəritəToolStripMenuItem.Text = "Xəritə";
+            xəritəToolStripMenuItem.Click += xəritəToolStripMenuItem_Click;
+            // 
+            // telefonToolStripMenuItem
+            // 
+            telefonToolStripMenuItem.BackColor = Color.Black;
+            telefonToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            telefonToolStripMenuItem.Image = (Image)resources.GetObject("telefonToolStripMenuItem.Image");
+            telefonToolStripMenuItem.Name = "telefonToolStripMenuItem";
+            telefonToolStripMenuItem.Size = new Size(180, 22);
+            telefonToolStripMenuItem.Text = "Telefon";
+            telefonToolStripMenuItem.Click += telefonToolStripMenuItem_Click;
+            // 
+            // emailToolStripMenuItem
+            // 
+            emailToolStripMenuItem.BackColor = Color.Black;
+            emailToolStripMenuItem.ForeColor = Color.FromArgb(255, 128, 0);
+            emailToolStripMenuItem.Image = (Image)resources.GetObject("emailToolStripMenuItem.Image");
+            emailToolStripMenuItem.Name = "emailToolStripMenuItem";
+            emailToolStripMenuItem.Size = new Size(180, 22);
+            emailToolStripMenuItem.Text = "Email";
+            emailToolStripMenuItem.Click += emailToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Silver;
+            ClientSize = new Size(658, 436);
+            Controls.Add(webView21);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Webview";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem sosialMediaToolStripMenuItem;
+        private ToolStripMenuItem instagramToolStripMenuItem;
+        private ToolStripMenuItem youtubeToolStripMenuItem;
+        private ToolStripMenuItem facebookToolStripMenuItem;
+        private ToolStripMenuItem telegramToolStripMenuItem;
+        private ToolStripMenuItem alətlərToolStripMenuItem;
+        private ToolStripMenuItem tamEkranToolStripMenuItem;
+        private ToolStripMenuItem kiçikEkranToolStripMenuItem;
+        private ToolStripMenuItem kopyalamaqToolStripMenuItem;
+        private ToolStripMenuItem yapışdırmaqToolStripMenuItem;
+        private ToolStripMenuItem əlaqəToolStripMenuItem;
+        private ToolStripMenuItem xəritəToolStripMenuItem;
+        private ToolStripMenuItem telefonToolStripMenuItem;
+        private ToolStripMenuItem emailToolStripMenuItem;
+        private ToolStripMenuItem youtubeToolStripMenuItem1;
+        private ToolStripMenuItem youtubeToolStripMenuItem2;
+    }
+}
---
+++ WebView/Form1.cs
@@ -0,0 +1,66 @@
+﻿namespace WebView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://my.beu.edu.az/");
+        }
+
+        private void youtubeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tamEkranToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void kiçikEkranToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void instagramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://www.instagram.com/");
+        }
+
+        private void youtubeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://www.youtube.com/");
+        }
+
+        private void facebookToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://www.facebook.com/");
+        }
+
+        private void telegramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://www.telegram.org/");
+        }
+
+        private void xəritəToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://www.google.com/maps");
+        }
+
+        private void telefonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("+994 77 555 22 21", "Telefon mömrəsi", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void emailToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://mail.google.com/mail/u/0/#inbox?compose=new");
+        }
+    }
+}
